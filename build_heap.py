60f5fbc63754ba0a6ec2e8594220017d64fceebe
Romāns Vēveris 221RDB094 <125798504+RomansVeveris@users.noreply.github.com>
2023-03-19 14:17:40 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -6,12 +6,33 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    n = len(data)
+    for i in range((n-1) // 2, -1, -1):
+        sift_down(data,i,swaps)
 
     return swaps
 
 
+
+def sift_down(data, i, swaps):
+    min_index = i
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
+    n = len(data)
+
+    if left_child <= n-1 and data[left_child] < data[min_index]:
+        min_index = left_child
+
+    if right_child <= n-1 and data[right_child] < data[min_index]:
+        min_index = right_child
+
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        sift_down(data, min_index, swaps)
+
 def main():
-    
+
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
